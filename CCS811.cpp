07b1d1458c5c479d2421577a86187d055c0c9544
Mary West <mary.west@sparkfun.com>
2017-08-25 15:53:58 -0600

    Adding documentation

---
+++ CCS811.cpp
@@ -0,0 +1,26 @@
+/**
+* Mary West @ SparkFun Electronics 
+* Ryan Mortenson https://github.com/ryanjmortenson 
+* August 25, 2017
+* https://github.com/ADataDate/pxt-CCS811
+*
+* Development environment specifics:
+* Written for Microsoft Makecode
+* Tested with a SparkFun weather:bit, CCS811 Air quality sensor for micro:bit
+*
+* This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+* Please review the LICENSE.md file included with this example. If you have any questions 
+* or concerns with licensing, please contact techsupport@sparkfun.com.
+* Distributed as-is; no warranty is given.
+*/
+
+
+#include "pxt.h"
+#include <cstdint>
+#include <math.h>
+
+using namespace pxt;
+
+namespace airQuality {
+   
+}
---
+++ CCS811.ts
@@ -0,0 +1,29 @@
+/**
+* Mary West @ SparkFun Electronics 
+* Ryan Mortenson https://github.com/ryanjmortenson
+* August 25, 2017
+* https://github.com/ADataDate/pxt-CCS811
+
+* Development environment specifics:
+* Written in Microsoft Makecode
+* Tested with a SparkFun weather:bit, CCS811 sensor for micro:bit
+*
+* This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+* Please review the LICENSE.md file included with this example. If you have any questions 
+* or concerns with licensing, please contact techsupport@sparkfun.com.
+* Distributed as-is; no warranty is given.
+*/
+
+
+/**
+ * Functions to operate the CCS811
+ */
+
+//% color=#33acff icon="\u27BE"
+namespace airQuality {
+ 
+
+  
+  
+   
+}

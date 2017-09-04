8b8e9709070ceda468e0eab0e63f279ec3a3065a
rmortens <ryan.j.mortenson@gmail.com>
2017-09-04 10:36:05 -0600

    Functional CCS811 code

--- airQuality.cpp
+++ airQuality.cpp
@@ -1,6 +1,6 @@
 /**
-* Mary West @ SparkFun Electronics 
-* Ryan Mortenson https://github.com/ryanjmortenson 
+* Mary West @ SparkFun Electronics
+* Ryan Mortenson https://github.com/ryanjmortenson
 * August 25, 2017
 * https://github.com/ADataDate/pxt-CCS811
 *
@@ -9,7 +9,7 @@
 * Tested with a SparkFun weather:bit, CCS811 Air quality sensor for micro:bit
 *
 * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
-* Please review the LICENSE.md file included with this example. If you have any questions 
+* Please review the LICENSE.md file included with this example. If you have any questions
 * or concerns with licensing, please contact techsupport@sparkfun.com.
 * Distributed as-is; no warranty is given.
 */
@@ -22,5 +22,5 @@
 using namespace pxt;
 
 namespace airQuality {
-   
+
 }

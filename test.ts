98245ea33e98e73d51d2180706d01d0fb17015c7
Mary West <mary.west@sparkfun.com>
2017-09-08 16:39:07 -0600

    added test file

--- test.ts
+++ test.ts
@@ -1,1 +1,14 @@
 // tests go here; this will not be compiled when this package is used as a library
+let eCO2 = 0
+let TVOCs = 0
+basic.forever(() => {
+    TVOCs = airQuality.readTvoc()
+    eCO2 = airQuality.readCo2()
+    basic.showString("TVOC")
+    basic.showNumber(TVOCs)
+    basic.pause(1000)
+    basic.showString("eCO2")
+    basic.showNumber(eCO2)
+    basic.pause(1000)
+})
+airQuality.appStart()
